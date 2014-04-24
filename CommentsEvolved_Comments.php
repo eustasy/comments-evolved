1e2b7fe614624a5a7b3a75a785f5b6b09e553409
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2014-04-24 21:54:02 +0100

    Fix Facebook App ID Not set

--- CommentsEvolved_Comments.php
+++ CommentsEvolved_Comments.php
@@ -61,7 +61,13 @@ function CommentsEvolved_Facebook($Canonical) {
 					$(\'#fb-replace\').html(\'<div class="fb-comments" data-width="'.$CommentsEvolved_Width.'" data-href="'.$Canonical.'" data-num-posts="10" data-colorscheme="light" data-mobile="auto"></div>\');
 				});
 			</script>
-			<script type="text/javascript" src="//connect.facebook.net/en_US/all.js#xfbml=1">FB.init();</script>
+			<script>(function(d, s, id) {
+			  var js, fjs = d.getElementsByTagName(s)[0];
+			  if (d.getElementById(id)) return;
+			  js = d.createElement(s); js.id = id;
+			  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=681781948553167";
+			  fjs.parentNode.insertBefore(js, fjs);
+			}(document, \'script\', \'facebook-jssdk\'));</script>
 			<noscript>Please enable JavaScript to view the <a href="https://www.facebook.com/">comments powered by Facebook.</a></noscript>
 		</div>';
 }
