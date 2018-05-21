72ffc31b259df925f9db21ddd61cac7ad8686534
Lewis Goddard <lewisgoddard@eustasy.org>
2018-05-21 21:39:41 +0100

    Update loadjquery.php

--- _functions/CommentsEvolved/loadjquery.php
+++ _functions/CommentsEvolved/loadjquery.php
@@ -2,7 +2,7 @@
 
 function CommentsEvolved_LoadjQuery(): string {
 	return '
-	<script src="https://cdn.jsdelivr.net/jquery/3/jquery.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/jquery.ui/1/jquery-ui.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/gh/jquery/jquery@3/dist/jquery.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/jquery-ui-dist@1/jquery-ui.min.js"></script>
 	<script>$(function(){$(\'.commentsevolved-tabs\').tabs()})</script>';
 }
